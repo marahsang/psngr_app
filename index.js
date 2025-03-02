cfb6fe0e7b8a1fdb9ebdc3d37b1a8c32655a8a43
Marah Sang <80205002+marahsang@users.noreply.github.com>
2025-03-02 13:59:49 +0000

    Create passenger app and commit

---
+++ index.js
@@ -0,0 +1,111 @@
+
+
+
+
+
+
+
+let saveEl = document.getElementById("save-el")
+let countEL = document.getElementById("count-el")
+ let count = 0
+ function increment() {
+     count += 1
+     countEL.textContent = count
+ }
+
+ function save() {
+     let countStr = count + "-"
+  //   saveEl.innerText += countStr
+     saveEl.textContent += countStr
+     countEL.textContent = 0
+     count = 0
+ }
+
+
+
+// let myPoints = 3
+
+// function add3Points() {
+//     myPoints += 3
+// }
+
+// function remove1Point() {
+//     myPoints -= 1
+// }
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+// add3Points()
+// console.log(myPoints)
+
+
+
+// let welcomeEL = document.getElementById("welcome-el")
+
+// let nam = "Abel"
+// let greeting = "Welcome back, "
+// message = greeting + nam
+// welcomeEL.innerText =  message
+// welcomeEL.innerText += " 😀"
+
+ 
+// let countEl = document.getElementById("count-el")
+// let couxEl = document.getElementById("coux-el")
+
+// let box = 50;
+// box = box - 25
+// box = box + 45
+// console.log(box)
+
+// /* let count = 0*/
+// let coux = 0
+// function coun () {
+// coux = coux + 1
+// couxEl.innerText = coux
+// }
+// coun()
+// let myPoints = 3
+// function add3Points() {
+// console.log(myPoints += 3)
+// }
+
+// function remove1Point() {
+// console.log(myPoints -= 1)
+// }
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+
+
+
+
+//  let countEl = document.getElementById("count-el")
+//  let saveEl = document.getElementById("save-el")
+//  let count = 0
+//  function increment (){
+//      count += 1
+//      countEl.textContent = count
+//  }
+
+
+// function save() {
+//     let countStr = count + " - "
+//     saveEl.textContent += countStr
+//      countEl.textContent = 0
+//     count = 0
+// }
+// save()
+
+
+
+// let countEL = document.getElementById("count-el")
+// let count = 0
+// function increment (){
+//     count += 1
+//     countEL.innerText = count
+// }
+// increment()
+ 
